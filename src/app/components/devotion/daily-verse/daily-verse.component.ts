5f9197cdf520d9bbcb0b51480760658f58d23f2f
Michael <kabalimichael03@gmail.com>
2020-02-14 09:42:13 +0300

    add devotion component

---
+++ src/app/components/devotion/daily-verse/daily-verse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-verse',
+  templateUrl: './daily-verse.component.html',
+  styleUrls: ['./daily-verse.component.css']
+})
+export class DailyVerseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
